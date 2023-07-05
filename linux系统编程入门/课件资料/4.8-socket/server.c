64977fedad73b0c28cd7ca3849971839ed64b360
QYCZCM <1071676923@qq.com>
2023-07-05 18:30:04 +0800

    update

--- linux系统编程入门/课件资料/4.8-socket/server.c
+++ linux系统编程入门/课件资料/4.8-socket/server.c
@@ -1,4 +1,28 @@
 //TCP通信发服务器端
 
-#include <arpa/inet.h>
+
+#include <windows.h>
+// #include <sys/types.h>
+// #include <sys/socket.h>
 #include <stdio.h>
+
+
+
+int main()
+{
+    //1.创建socket（用于监听的套接字）
+    //AF_INET:ipv4  SOCK_STREAM:流式协议
+    int lfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(lfd == -1){
+        perror("Creat_Socket");
+    }
+
+    //2.绑定
+    struct sockaddr_in saddr;
+    inet_pton()
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.S_addr = 
+    bind(lfd,)
+
+    return 0;
+}
