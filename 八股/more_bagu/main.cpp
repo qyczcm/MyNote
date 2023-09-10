5b67641970180ed8efc458c96bcd219a064ef41b
QYCZCM <1071676923@qq.com>
2023-09-10 18:32:30 +0800

    update

--- 八股/more_bagu/main.cpp
+++ 八股/more_bagu/main.cpp
@@ -1,48 +1,105 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
-string findLongestSubstring(const string& str) {
-    int maxLength = 0; // 最长子字符串的长度
-    int start = 0; // 最长子字符串的起始位置
+int countSubsets(vector<int>& nums, int k) {
+    int count = 0;
+    int n = nums.size();
 
-    for (int i = 0; i < str.length(); i++) {
-        int length = 1; // 当前子字符串的长度
+    for (int i = 0; i < (1 << n); i++) {
+        vector<int> subset;
 
-        // 探测当前字符与后续字符是否相同
-        while (i + length < str.length() && str[i] == str[i + length]) {
-            length++;
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                subset.push_back(nums[j]);
+            }
         }
 
-        // 更新最长子字符串的信息
-        if (length > maxLength) {
-            maxLength = length;
-            start = i;
+        bool valid = true;
+        for (int x = 0; x < subset.size(); x++) {
+            for (int y = x + 1; y < subset.size(); y++) {
+                if (abs(subset[x] - subset[y]) == k) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid) {
+                break;
+            }
+        }
+
+        if (valid) {
+            count++;
         }
     }
 
-    return str.substr(start, maxLength);
+    return count;
 }
 
-int main() {
-    string str="1900:5,5800:45,4998:45";
-    string num;
-    vector<int> freg;
-    int fregnum = 0;
-    for(auto c : str){
-        if(isdigit(c)){
-            num += c;
+int checkAbsoluteDifference(vector<int>& nums, int k) {
+    unordered_set<int> diffs; // 使用哈希集合记录已经出现过的绝对差
+    for (int num : nums) {
+        if (diffs.count(num - k) || diffs.count(k - num)) {
+            return 0;
         }
-        else if(c == ':'){
-            int temp = stoi(num);
-            cout << temp << endl;
-            freg.push_back(temp);
-            num = "";
-        }
+        diffs.insert(abs(num - k));
+        diffs.insert(abs(num + k));
     }
-    cout<< freg[0];
+    return 1;
+}
+
+
+int main() {
+    vector<int> nums = {1,3};
+    int k = 2;
+    int a = checkAbsoluteDifference(nums,k);
+    cout << a << endl;
+    ;
+    int result = countSubsets(nums, k);
+    cout << result << endl;
 
     return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+
+// using namespace std;
+
+// int countSubsets(vector<int>& nums, int k) {
+//     int n = nums.size();
+
+//     // dp[i][j] 表示使用前 i 个数，绝对差为 j 的子集数量
+//     vector<unordered_map<int, int>> dp(n + 1);
+//     dp[0][0] = 1; // 初始化
+
+//     for (int i = 1; i <= n; i++) {
+//         dp[i] = dp[i - 1]; // 继承前一个状态的子集数量
+//         for (auto& [diff, count] : dp[i - 1]) {
+//             int newDiff = abs(nums[i - 1] - diff);
+//             dp[i][newDiff] += count;
+//         }
+//     }
+
+//     int count = 0;
+//     for (auto& [diff, subsetCount] : dp[n]) {
+//         if (diff == k) {
+//             count += subsetCount;
+//         }
+//     }
+
+//     return count;
+// }
+
+// int main() {
+//     vector<int> nums = {2, 4, 6};
+//     int k = 2;
+
+//     int result = countSubsets(nums, k);
+//     cout << result << endl;
+
+//     return 0;
+// }
+
